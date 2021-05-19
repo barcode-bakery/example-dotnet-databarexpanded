a1b5a319242ea018b5928ae69a61e80d152dda39
Jean-Sébastien Goupil <contact@jsgoupil.com>
2021-05-19 15:36:19 -0700

    Initial commit.

---
+++ example/BarcodeAztec.Core.Console/ExampleDatabarExpanded.cs
@@ -0,0 +1,55 @@
+﻿using BarcodeBakery.Barcode;
+using BarcodeBakery.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace BarcodeDatabarExpanded.Core.Console
+{
+    public class ExampleDatabarExpanded
+    {
+        public static async Task CreateAsync(string filePath, string? text = null)
+        {
+            // Loading Font
+            var font = new BCGFont("Arial", 18);
+
+            // Don't forget to sanitize user inputs
+            text = text?.Length > 0 ? text : "01900123456789083103001750";
+
+            // The arguments are R, G, B for color.
+            var colorBlack = new BCGColor(0, 0, 0);
+            var colorWhite = new BCGColor(255, 255, 255);
+
+            Exception? drawException = null;
+            BCGBarcode? barcode = null;
+            try
+            {
+                var code = new BCGdatabarexpanded();
+                code.SetScale(1); // Resolution
+                code.SetStacked(1);
+                code.SetForegroundColor(colorBlack); // Color of bars
+                code.SetBackgroundColor(colorWhite); // Color of spaces
+                code.SetFont(font); // Font
+                code.SetLinkageFlag(false);
+                code.Parse(text);
+                barcode = code;
+            }
+            catch (Exception exception)
+            {
+                drawException = exception;
+            }
+
+            var drawing = new BCGDrawing(barcode, colorWhite);
+            if (drawException != null)
+            {
+                drawing.DrawException(drawException);
+            }
+
+            // Saves the barcode into a file.
+            await drawing.FinishAsync(BCGDrawing.ImageFormat.Png, filePath);
+
+            // Saves the barcode into a MemoryStream
+            ////var memoryStream = new System.IO.MemoryStream();
+            ////await drawing.FinishAsync(BCGDrawing.ImageFormat.Png, memoryStream);
+        }
+    }
+}
---
+++ example/BarcodeAztec.Core.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace BarcodeDatabarExpanded.Core.Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // See each barcode file to see how you can save to a file or a MemoryStream.
+            await ExampleDatabarExpanded.CreateAsync("barcode_databarexpanded.png");
+        }
+    }
+}
